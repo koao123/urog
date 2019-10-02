f75c9862678bba4a5723b4abdfbda2ebce7f99fc
wat <monocle888@gmail.com>
2019-10-02 13:52:03 +0000

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  root 'posts#index'
-  get 'posts#index'
+  get 'users/new'
+
+  root 'static_pages#home'
+  get  '/help',    to: 'static_pages#help'
+
+ get '/signup', to: 'users/new'
+ 
+  get 'posts/index' => 'posts#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
