625c40a37e73ab7d2f24ad419e65bd66be5e9f2d
wat <monocle888@gmail.com>
2019-10-03 13:01:12 +0000

    Finish create posts model controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  root 'posts#index'
+  resources :posts
 
-  root 'static_pages#home'
-  get  '/help',    to: 'static_pages#help'
-
- get '/signup', to: 'users/new'
- 
-  get 'posts/index' => 'posts#index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
